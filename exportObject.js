71a3552056ca590ade66ec72f36cf35c0cc9af9d
sandeep <sundeep2258@gmail.com>
2022-07-01 04:06:12 +0000

    common module object

---
+++ exportObject.js
@@ -0,0 +1,7 @@
+const fullName = { firstName: "John", lastName: "Wilson" };
+module.exports = fullName;
+
+// Write an object with keys as firstName and lastName and values as John and Wilson respectively in the file exportObject.js and export it using the default export syntax.
+
+// Import the object in the importObject.js file.
+// ccbp submit NJSCPJLEPF
